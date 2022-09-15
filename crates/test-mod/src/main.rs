b2d27b390a00a2db7b789ab67d3ce1a2ea668a6e
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-09-14 22:02:24 -0400

    rename to example mod

--- crates/test-mod/src/main.rs
+++ crates/test-mod/src/main.rs
@@ -3,7 +3,7 @@ use rustic_craft::MinecraftMod;
 fn main() {
     println!("Creating test mod...");
 
-    let test_mod = MinecraftMod::new("rustic-craft-test-mod");
+    let test_mod = MinecraftMod::new("rustic-craft-example-mod");
 
     rustic_craft::create_mod_json_file(test_mod).expect("temporary handling");
 }
